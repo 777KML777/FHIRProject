c694d42717249dcd37c6f7e379bdfacbf36db7fa
Kleber777 <klebermirandalima@gmail.com>
2023-09-22 14:46:34 -0300

    Subindo primeira versão do projeto

---
+++ src/app/pagina-teste/page.js
@@ -0,0 +1,11 @@
+import ComponenteTeste from "../components/ComponenteTeste/ComponenteTeste";
+
+export default function Teste() {
+    return (
+      <>
+        {/* Aqui você irá importar os seus componentes criados */}
+        <ComponenteTeste/>        
+      </>
+    )
+  }
+  
