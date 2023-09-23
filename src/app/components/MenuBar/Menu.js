036d383c44f5f700579c29d297dfe36e16acbbea
João Pedro <joaopedrotranquilinobarbosa@gmail.com>
2023-09-22 21:04:44 -0300

    Criei meus componentes

---
+++ src/app/components/MenuBar/Menu.js
@@ -0,0 +1,13 @@
+import "../MenuBar/menu.css"; /* Não esquecer de importar o CSS do seu componente */
+
+// Trocar para o nome do seu componente
+export default function Menu() {
+  return (
+    <>
+      {/* Aqui você irá colocar o seu código HTML para usar em qualquer página */}
+      <div className="menu">
+        <h3>Meu menu</h3>
+      </div>
+    </>
+  );
+}
--- src/app/pagina-teste/page.js
+++ src/app/pagina-teste/page.js
@@ -1,11 +1,14 @@
 import ComponenteTeste from "../components/ComponenteTeste/ComponenteTeste";
+import Formulario from "../components/Formulario/Formulario";
+import Menu from "../components/MenuBar/menu";
 
 export default function Teste() {
     return (
       <>
         {/* Aqui você irá importar os seus componentes criados */}
-        <ComponenteTeste/>        
+        {/* <ComponenteTeste/> */}
+        <Menu/>
+        <Formulario/>     
       </>
     )
   }
-  
