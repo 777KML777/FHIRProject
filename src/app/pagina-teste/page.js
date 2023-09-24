cfd92f2da80d62109ba47d920e7b9990c1ab7cfe
Kleber Miranda Lima <klebermirandalima@MacBook-Air-de-Kleber.local>
2023-09-23 22:25:16 -0300

    Terminando primeira versão do componente Receita de tabela de receita

--- src/app/pagina-teste/page.js
+++ src/app/pagina-teste/page.js
@@ -1,16 +1,12 @@
-import ComponenteTeste from "../components/Exemplo/ComponenteTeste";
-import Formulario from "../components/Formulario/Formulario";
-import Menu from "../components/MenuBar/Menu";
-import Footer from "../components/Footer/Footer";
+import Receita from "../components/Receitas/Receita";
+import '../database/mock'
 
 export default function Teste() {
-    return (
-      <>
-        {/* Aqui você irá importar os seus componentes criados */}
-        {/* <ComponenteTeste/> */}
-        <Menu/> 
-        <Formulario/>
-        <Footer/>  
-      </>
-    )
-  }
+  return (
+    <>
+      <Receita
+        props={Receitas}
+      />
+    </>
+  )
+}
