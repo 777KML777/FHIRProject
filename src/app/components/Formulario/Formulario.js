b07c679d6e47d364b8bed958dfb5e61c8b3b56a6
João Pedro <joaopedrotranquilinobarbosa@gmail.com>
2023-09-28 20:43:52 -0300

    Componente formulario quase pronto, falta pequenos ajustes

--- src/app/components/Formulario/Formulario.js
+++ src/app/components/Formulario/Formulario.js
@@ -5,59 +5,69 @@ export default function Formulario() {
   return (
     <>
       {/* Aqui você irá colocar o seu código HTML para usar em qualquer página */}
-      <div className="box">
-        <p>Dados do Beneficiário</p>
-        <form action="">
-          <fieldset>
-            <div className="inputBox">
-              <label for="idcpf">CPF</label>
-              <input type="number" name="idcpf" id="idcpf" className="inputUser" required></input>
+      <div className="form">
+        <form action="#">
+          <div className="form-header">
+            <div className="title">
+              <h1>Dados do Beneficiário</h1>
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="input-box">
+              <label for="cpf">CPF</label>
+              <input id="cpf" type="number" name="cpf" placeholder="000.000.000-00" required/>
             </div>
 
-            <div className="inputBox">
-              <label for="idnome">Nome</label>
-              <input type="text" name="idnome" id="idnome" className="inputUser" required></input>
+            <div className="input-box">
+              <label for="nome">Nome</label>
+              <input id="nome" type="text" name="nome" placeholder="Digite o seu nome" required/>
             </div>
 
-            <div className="inputBox">
-              <label for="iddate">Data de Nascimento</label>
-              <input type="date" name="iddate" id="iddate" className="inputUser" required></input>
+            <div className="input-box">
+              <label for="date">Data de Nascimento</label>
+              <input id="date" type="date" name="date" required/>
             </div>
 
-            <div className="inputBox">
-              <label for="idmatricula">Matrícula</label>
-              <input type="number" name="idmatricula" id="idmatricula" className="inputUser" required></input>
+            <div className="input-box">
+              <label for="matricula">Matrícula</label>
+              <input id="matricula" type="number" name="matricula" placeholder="000000000" required/>
             </div>
 
-            <div className="inputBox">
-              <label for="idnmresponsavel">Nome do Responsável</label>
-              <input type="text" name="idnmresponsavel" id="idnmresponsavel" className="inputUser" required></input>
+            <div className="input-box">
+              <label for="nmresponsável">Nome do Responsável</label>
+              <input id="nmresponsável" type="text" name="nmresponsável" placeholder="Digite o nome do responsável"/>
             </div>
 
-            <div className="inputBox">
-              <label for="idendereco">Endereço</label>
-              <input type="text" name="idendereco" id="idendereco" className="inputUser" required></input>
+            <div className="input-box">
+              <label for="endereco">Endereço</label>
+              <input id="endereco" type="text" name="endereco" placeholder="Rua Chica da Silva 222, Universitário,Itaúna, MG - 35681-150" required/>
             </div>
 
-            <div className="inputBox">
-              <label for="idtelefone">Telefone</label>
-              <input type="tel" name="idtelefone" id="idtelefone" className="inputUser" required></input>
+            <div className="input-box">
+              <label for="celular">Celular</label>
+              <input id="celular" type="tel" name="celular" placeholder="(xx) xxxx-xxxx" required/>
             </div>
 
-            <div className="inputBox">
-              <label for="idemail">E-mail</label>
-              <input type="email" name="idemail" id="idemail" className="inputUser" required></input>
+            <div className="input-box">
+              <label for="email">E-mail</label>
+              <input id="email" type="email" name="email" placeholder="Digite seu email" required/>
             </div>
-          </fieldset>
+
+
+            <div className="buttons-dados">
+              <div className="alt-dados-cad">
+                <p>ALTERAR DADOS CADASTRADOS</p>
+                <img src="" alt=""/>
+              </div>
+              <div className="del-cadastro">
+                <p>DELETAR CADASTRO</p>
+                <img src="" alt=""/>
+              </div>
+            </div>
+
+          </div>
         </form>
       </div>
-
-
-
-
-
-
-
     </>
   );
 }
---
+++ src/app/pagina-chamada/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Chamada() {
+    return (
+      <>
+        <h1>Nova página</h1>
+      </>
+    )
+  }
