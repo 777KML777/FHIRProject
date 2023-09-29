17d4cdce3109b489968d81f707e0630f96fc5098
João Pedro <joaopedrotranquilinobarbosa@gmail.com>
2023-09-29 17:29:52 -0300

    Formulario finalizado e responsiva

--- src/app/components/Formulario/Formulario.js
+++ src/app/components/Formulario/Formulario.js
@@ -5,69 +5,69 @@ export default function Formulario() {
   return (
     <>
       {/* Aqui você irá colocar o seu código HTML para usar em qualquer página */}
-      <div className="form">
-        <form action="#">
-          <div className="form-header">
-            <div className="title">
-              <h1>Dados do Beneficiário</h1>
-            </div>
+      
+      <section className="dados-beneficiario">
+        <div className="titulo">
+          <h2>Dados do Beneficiário</h2>
+        </div>
+        
+        <div className="dados">
+          <div className="cpf">
+            <p>CPF</p>
+            <p>000.000.000-00</p>
+            
           </div>
-          <div className="input-group">
-            <div className="input-box">
-              <label for="cpf">CPF</label>
-              <input id="cpf" type="number" name="cpf" placeholder="000.000.000-00" required/>
-            </div>
-
-            <div className="input-box">
-              <label for="nome">Nome</label>
-              <input id="nome" type="text" name="nome" placeholder="Digite o seu nome" required/>
-            </div>
-
-            <div className="input-box">
-              <label for="date">Data de Nascimento</label>
-              <input id="date" type="date" name="date" required/>
-            </div>
-
-            <div className="input-box">
-              <label for="matricula">Matrícula</label>
-              <input id="matricula" type="number" name="matricula" placeholder="000000000" required/>
-            </div>
-
-            <div className="input-box">
-              <label for="nmresponsável">Nome do Responsável</label>
-              <input id="nmresponsável" type="text" name="nmresponsável" placeholder="Digite o nome do responsável"/>
-            </div>
-
-            <div className="input-box">
-              <label for="endereco">Endereço</label>
-              <input id="endereco" type="text" name="endereco" placeholder="Rua Chica da Silva 222, Universitário,Itaúna, MG - 35681-150" required/>
-            </div>
-
-            <div className="input-box">
-              <label for="celular">Celular</label>
-              <input id="celular" type="tel" name="celular" placeholder="(xx) xxxx-xxxx" required/>
-            </div>
-
-            <div className="input-box">
-              <label for="email">E-mail</label>
-              <input id="email" type="email" name="email" placeholder="Digite seu email" required/>
-            </div>
-
-
-            <div className="buttons-dados">
-              <div className="alt-dados-cad">
-                <p>ALTERAR DADOS CADASTRADOS</p>
-                <img src="" alt=""/>
-              </div>
-              <div className="del-cadastro">
-                <p>DELETAR CADASTRO</p>
-                <img src="" alt=""/>
-              </div>
-            </div>
+          <div className="nome">
+            <p>Nome</p>
+            <p>Kleber Martins Figueredo</p>
 
           </div>
-        </form>
-      </div>
+          <div className="data-nasc">
+            <p>Data de Nascimento</p>
+            <p>00/00/0000</p>
+            
+          </div>
+          <div className="matricula">
+            <p>Matrícula</p>
+            <p>000000000</p>
+
+          </div>
+          <div className="nome-resp">
+            <p>Nome do Responsével</p>
+            <p>Fulano De Fulano De Fulano</p>
+            
+          </div>
+          <div className="endereco">
+            <p>Endereço</p>
+            <p>Rua Chica da Silva 222, Universitário, Itaúna, MG - 35681-150 </p>
+            
+          </div>
+          <div className="telefone">
+            <p>Telefone</p>
+            <p>(xx) xxxx-xxxx</p>
+          
+          </div>
+          <div className="email">
+            <p>E-mail</p>
+            <p>FulanoFulanoFulano@Gmail.com</p>
+          
+          </div>
+        </div>
+
+        <div className="buttons-dados">
+          <button className="alt-dados-cad">ALTERAR DADOS CADASTRADOS
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none">
+            <path d="M23.2671 0.937304C22.1866 -0.143164 20.4401 -0.143164 19.3596 0.937304L17.8746 2.4174L22.7046 7.24743L24.1896 5.76241C25.2701 4.68194 25.2701 2.93543 24.1896 1.85496L23.2671 0.937304ZM8.5056 11.7913C8.20465 12.0923 7.97277 12.4623 7.83956 12.8718L6.3792 17.2529C6.23612 17.6772 6.3496 18.1458 6.66535 18.4665C6.9811 18.7872 7.4498 18.8958 7.87903 18.7527L12.2601 17.2923C12.6647 17.1591 13.0347 16.9272 13.3406 16.6263L21.5946 8.36737L16.7596 3.5324L8.5056 11.7913ZM4.7363 3.02423C2.12147 3.02423 0 5.1457 0 7.76053V20.3907C0 23.0055 2.12147 25.127 4.7363 25.127H17.3664C19.9813 25.127 22.1027 23.0055 22.1027 20.3907V15.6544C22.1027 14.7811 21.3972 14.0756 20.524 14.0756C19.6507 14.0756 18.9452 14.7811 18.9452 15.6544V20.3907C18.9452 21.2639 18.2397 21.9694 17.3664 21.9694H4.7363C3.86304 21.9694 3.15753 21.2639 3.15753 20.3907V7.76053C3.15753 6.88728 3.86304 6.18177 4.7363 6.18177H9.47259C10.3458 6.18177 11.0514 5.47625 11.0514 4.603C11.0514 3.72975 10.3458 3.02423 9.47259 3.02423H4.7363Z" fill="white"/>
+          </svg>
+          </button>
+          <button className="del-cadastro">DELETAR CADASTRO 
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none">
+            <path d="M23.2671 0.937304C22.1866 -0.143164 20.4401 -0.143164 19.3596 0.937304L17.8746 2.4174L22.7046 7.24743L24.1896 5.76241C25.2701 4.68194 25.2701 2.93543 24.1896 1.85496L23.2671 0.937304ZM8.5056 11.7913C8.20465 12.0923 7.97277 12.4623 7.83956 12.8718L6.3792 17.2529C6.23612 17.6772 6.3496 18.1458 6.66535 18.4665C6.9811 18.7872 7.4498 18.8958 7.87903 18.7527L12.2601 17.2923C12.6647 17.1591 13.0347 16.9272 13.3406 16.6263L21.5946 8.36737L16.7596 3.5324L8.5056 11.7913ZM4.7363 3.02423C2.12147 3.02423 0 5.1457 0 7.76053V20.3907C0 23.0055 2.12147 25.127 4.7363 25.127H17.3664C19.9813 25.127 22.1027 23.0055 22.1027 20.3907V15.6544C22.1027 14.7811 21.3972 14.0756 20.524 14.0756C19.6507 14.0756 18.9452 14.7811 18.9452 15.6544V20.3907C18.9452 21.2639 18.2397 21.9694 17.3664 21.9694H4.7363C3.86304 21.9694 3.15753 21.2639 3.15753 20.3907V7.76053C3.15753 6.88728 3.86304 6.18177 4.7363 6.18177H9.47259C10.3458 6.18177 11.0514 5.47625 11.0514 4.603C11.0514 3.72975 10.3458 3.02423 9.47259 3.02423H4.7363Z" fill="white"/>
+            </svg>
+          </button>
+        </div>
+      </section>
+
     </>
   );
 }
