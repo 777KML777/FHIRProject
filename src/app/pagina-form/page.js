ee67c28438ea111c9bbad136e7360f1f1f36200a
João Pedro <joaopedrotranquilinobarbosa@gmail.com>
2023-09-29 21:29:35 -0300

    Preparendo componentes

--- src/app/pagina-form/page.js
+++ src/app/pagina-form/page.js
@@ -1,11 +1,11 @@
-import DadosBeneficiario from "../components/DadosBeneficiario/DadosBeneficiario";
+import Formulario from "../components/Formulario/Formulario";
 
 export default function Teste() {
     return (
       <>
         {/* Aqui você irá importar os seus componentes criados */}
         {/* <ComponenteTeste/> */}
-        <DadosBeneficiario/>
+        <Formulario/>
       </>
     )
   }
