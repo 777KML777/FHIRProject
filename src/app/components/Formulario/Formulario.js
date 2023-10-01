23925a1d7e998a9fed66510bb27d5bbde4d14df2
João Pedro <joaopedrotranquilinobarbosa@gmail.com>
2023-10-01 17:24:16 -0300

    Formulário finalizado e responsivo

--- src/app/components/Formulario/Formulario.js
+++ src/app/components/Formulario/Formulario.js
@@ -5,42 +5,34 @@ export default function Formulario() {
   return (
     <>
       {/* Aqui você irá colocar o seu código HTML para usar em qualquer página */}
-      <form action="#">
-        <div className="form-header">
-          <div className="title">
-            <h1>Cadastre-se gratuitamente!</h1>
+      <div className="form">
+        <form action="#">
+          <div className="form-header">
+            <h2 className="titulo">
+              Cadastre-se gratuitamente!
+            </h2>
           </div>
-        </div>
-        
-        <div className="input-group">
-          <div className="input-box">
-            <input id="first-name" type="text" name="first-name" placeholder="Primeiro Nome" required/>
-          </div>
-
-          <div className="input-box">
-            <input id="second-name" type="text" name="second-name" placeholder="Segundo Nome" required/>
-          </div>
-
-          <div className="input-box">
-            <input id="email" type="email" name="email" placeholder="E-mail" required/>
-          </div>
-
-          <div className="input-box">
-            <input id="password" type="password" name="password" placeholder="Senha" required/>
-          </div>
-
-          <div className="buttons-dados">
-            <div className="cadastro">
-              <p>Cadastre-se!</p>
+          <div className="input-group">
+            <div className="nome">
+              <div className="input-box">
+                <input id="first-name" type="text" name="first-name" placeholder="Primeiro Nome" required/>
+              </div>
+              <div className="input-box">
+                <input id="second-name" type="text" name="second-name" placeholder="Segundo Nome" required/>
+              </div>
+            </div>
+            <div className="input-box">
+              <input id="email" type="email" name="email" placeholder="E-mail" required/>
+            </div>
+            <div className="input-box">
+              <input id="password" type="password" name="password" placeholder="Senha" required/>
+            </div>
+            <div className="cadastro-button">
+              <button>Cadastre-se!</button>
             </div>
           </div>
-        </div>
-      </form>
-
-
-
-
-    
+        </form>
+      </div>
     </>
   );
 }
