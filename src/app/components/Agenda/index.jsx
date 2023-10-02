41e4b83fbd2089285169658a8eeeea506a2ad606
Leonardo de Souza <leonardodesouzaalmeidas@gmail.com>
2023-10-02 13:10:36 -0300

    Estado Categoria Consultas Criados

---
+++ src/app/components/Agenda/index.jsx
@@ -0,0 +1,14 @@
+import styles from './agenda.module.css'
+import Estados from './estados'
+import Categorias from './categorias'
+import Consultas from './consultas'
+
+function Agenda(){
+    return(
+        <>
+            <Consultas/>
+        </>
+    )
+}
+
+export default Agenda
--- src/app/components/AgendarConsultas/Filtros/FiltroItem/index.jsx
+++ src/app/components/AgendarConsultas/Filtros/FiltroItem/index.jsx
@@ -24,7 +24,6 @@ function FiltroItem({ filtros }) {
                                 : ""}
                         </div>
                     </div>
-
                 )
             })}
         </>
