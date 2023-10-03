9552c4d01195df983e5111e4ef43e97dfb53eed7
João Pedro <joaopedrotranquilinobarbosa@gmail.com>
2023-10-03 19:01:02 -0300

    Finalizei o componente Profissionais

--- src/app/components/Home/NossosProfissionais/index.jsx
+++ src/app/components/Home/NossosProfissionais/index.jsx
@@ -10,25 +10,27 @@ export default function NossosProfissionais() {
               <p>Conheça nossa equipe de especialistas em saúde mental prontos para ajudar você a cuidar da sua mente e emoções.</p>
             </div>
             
-            <div className={styles.doutores_foto}>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className={styles.doutores_foto}>  
+              <div className={styles.icone_doc}><img src="https://i.imgur.com/bqXHIGw.png" alt="Doutor" /></div>
+              <div className={styles.icone_doc}><img src="https://i.imgur.com/J6ro0aF.png" alt="Doutor" /></div>
+              <div className={styles.icone_doc}><img src="https://i.imgur.com/m2eOQCF.png" alt="Doutor" /></div>
+              <div className={styles.icone_doc}><img src="https://i.imgur.com/8HWCd4j.png" alt="Doutor" /></div>
+              <div className={styles.icone_doc}><img src="https://i.imgur.com/GsWwFgf.png" alt="Doutor" /></div>
+              <div className={styles.icone_doc}><img src="https://i.imgur.com/squCHoa.png" alt="Doutor" /></div>
             </div>
           </div>
           <div className={styles.profissional_selecionado}>
               <div className={styles.img_profissional}></div>
-              <h1>Gabriela</h1>
-              <p>A minha missão é promover o bem-estar emocional 
-                e mental dos meus clientes. Através de terapia 
-                personalizada, ofereço um espaço seguro para 
-                explorar pensamentos, emoções e desafios, 
-                auxiliando no desenvolvimento de habilidades de 
-                enfrentamento saudáveis e no alcance de uma vida 
-                mais equilibrada e feliz.</p>
+              <div className={styles.sobre_profissional}>
+                <h1>Gabriela</h1>
+                <p>A minha missão é promover o bem-estar emocional 
+                  e mental dos meus clientes. Através de terapia 
+                  personalizada, ofereço um espaço seguro para 
+                  explorar pensamentos, emoções e desafios, 
+                  auxiliando no desenvolvimento de habilidades de 
+                  enfrentamento saudáveis e no alcance de uma vida 
+                  mais equilibrada e feliz.</p>
+              </div>
             </div>
         </section>
 
