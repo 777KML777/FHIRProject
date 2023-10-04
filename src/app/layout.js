4f3d3463a24c7f67085dc2be5970717bd90b1f0d
João Pedro <joaopedrotranquilinobarbosa@gmail.com>
2023-10-04 14:44:58 -0300

    Footer finalizado

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/app/components/Header";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
