4da5036de7b5133adfcd7ce7cdf241e4075f2f17
Kleber777 <klebermirandalima@gmail.com>
2023-10-05 10:24:15 -0300

    Desacoplando a api do front. Inserindo request publica para acesso externo a api

--- src/app/api/requests.js
+++ src/app/api/requests.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const request = {
     localDebug: "http://localhost:3000",
+    publishAPI: "https://fhir-project-api.vercel.app",
     envDev: ""
 }
 
 export default axios.create({
-    baseURL : request.localDebug
+    baseURL : request.publishAPI
 })
