686f03f41d21f8ec16c60e15a87e6ed7ca078106
Leonardo de Souza <leonardodesouzaalmeidas@gmail.com>
2023-10-05 17:51:16 -0300

    Componente Modal Criado

--- src/app/components/Agenda/index.jsx
+++ src/app/components/Agenda/index.jsx
@@ -1,13 +1,23 @@
+"use client";
 import styles from './agenda.module.css'
+
+import React from 'react';
+import { useState } from 'react';
+
 import Estados from './estados'
 import Categorias from './categorias'
 import Consultas from './consultas'
 import Calendario from '../AgendarConsultas/Calendario'
 import Tarefas from './tarefas'
+import Modal from '../Modal'
 
 function Agenda() {
+    const [ativo, setAtivo] = useState("True")
+    
+    console.log(Categorias)
     return (
         <>
+           
             <div className={styles.container}>
                 <div className={styles.side}>
                     <Calendario />
@@ -19,7 +29,10 @@ function Agenda() {
                     <Tarefas />
                 </div>
             </div>
-
+            <Modal classe={ativo} tela={Categorias}>
+                <Categorias /> 
+                {/* Substitua Categorias, pelo componente que será exibido no modal */}
+            </Modal>
         </>
     )
 }
